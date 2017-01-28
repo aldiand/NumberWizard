7532d85373cdc3872e78a7d7b4c5550a0190b4e7
DESKTOP-P213RP4\super <supersandre@gmail.com>
2017-01-29 03:48:21 +0700

    Number Wizard Game Unity

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level requested : " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("I want to quit the game");
+        Application.Quit();
+    }
+}
---
+++ Assets/NumberWizards.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+	int max;
+	int min;
+    int guess;
+    int maxGuess = 5;
+    public Text text;
+   
+	// Use this for initialization
+	void Start ()
+    {
+		StartGame ();
+	}
+
+	void StartGame()
+    {
+		max = 1000;
+		min = 1;
+		guess = 500;
+		max = max + 1;
+	}
+
+	void NextGuess()
+    {
+		guess = (max + min) / 2;
+        text.text = guess.ToString();
+        maxGuess--;
+        if(maxGuess < 0)
+        {
+            SceneManager.LoadScene("Win");
+        }
+	}
+
+    public void GuestHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuestLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+}
